33fb94adc6b133ced0dfdddefcd12c8203f32ba3
Eduardo Quiroga <quirogagis@gmail.com>
2023-12-03 14:47:26 -0300

    Coneccion a la base de datos mongodb

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import createHttpError from "http-errors";
+import routes from "./routes/index.js";
 
 // Configuración de dotenv
 dotenv.config();
@@ -51,8 +53,22 @@ app.use(
   })
 );
 
-app.post("/test", (req, res) => {
-  res.json(req.body);
+// api v1 routes
+app.use("/api/v1", routes);
+
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist"));
+});
+
+// error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
-import app from './app.js'
-import logger from './configs/logger.config.js';
-
-
+import app from "./app.js";
+import logger from "./configs/logger.config.js";
+import mongoose from "mongoose";
 
 // variables de entorno
-const PORT = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV)
+const { DATABASE_URL } = process.env;
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8000;
+console.log(process.env.NODE_ENV);
+
+// exit on mongodb error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Error de coneccion: ${err}`);
+  process.exit(1);
+});
+
+// mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+// mongodb connection
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Conectado a la base de datos MongoDB");
+  });
+
+let server;
+
+server = app.listen(PORT, () => {
   logger.info(`Con Logger Escuchando Puerto ${PORT}...`);
+  // throw new Error("error in server");
+  console.log("process id", process.pid);
+});
+
+// handle server errors
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server Cerrado");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+process.on("uncaughtException", unexpectedErrorHandler);
+
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server Closed");
+    process.exit(1);
+  }
 });
